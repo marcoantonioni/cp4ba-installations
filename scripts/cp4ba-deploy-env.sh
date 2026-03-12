97a6b9020529a399a869a729a72e560c4cfc3974
marcoantonioni <antonioni.marco@gmail.com>
2026-03-12 16:27:34 +0100

    update

--- scripts/cp4ba-deploy-env.sh
+++ scripts/cp4ba-deploy-env.sh
@@ -565,7 +565,7 @@ zenCertInTrustedList () {
 
 waitDeploymentReadiness () {
   echo -e "${_CLR_YELLOW}==============================================================${_CLR_NC}"
-  echo -e "${_CLR_GREEN}Configuration and deployment complete for CR '${_CLR_YELLOW}${CP4BA_INST_CR_NAME}${_CLR_GREEN}'${_CLR_NC}"
+  echo -e "${_CLR_GREEN}Configuration and deployment complete for CR '${_CLR_YELLOW}${CP4BA_INST_CR_NAME}${_CLR_GREEN}' in  namespace '${_CLR_YELLOW}${CP4BA_INST_NAMESPACE}${_CLR_GREEN}'${_CLR_NC}"
 
   _seconds=0
   _total_warnings=0
