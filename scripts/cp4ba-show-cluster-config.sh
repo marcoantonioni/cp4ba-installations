f1699206cd8d38c0a71de05672dd75748ed3a5f1
marcoantonioni <antonioni.marco@gmail.com>
2026-06-11 09:45:30 +0200

    update

--- scripts/cp4ba-show-cluster-config.sh
+++ scripts/cp4ba-show-cluster-config.sh
@@ -37,6 +37,7 @@ checkPrereqTools () {
 }
 
 showClusterConfiguration () {
+
   _CM_CLUS_CFG=$(oc get configmap cluster-config-v1 -n kube-system -o jsonpath='{.data.install-config}' | yq -o json)
 
   if [[ ! -z "${_CM_CLUS_CFG}" ]]; then
@@ -51,7 +52,7 @@ showClusterConfiguration () {
     _WN_HYPERT=$(echo $_CM_CLUS_CFG | jq .controlPlane.hyperthreading | sed 's/"//g')
     _WN_NAME=$(echo $_CM_CLUS_CFG | jq .controlPlane.name | sed 's/"//g')
     _WN_REPL=$(echo $_CM_CLUS_CFG | jq .controlPlane.replicas | sed 's/"//g')
-    echo -e "${_CLR_GREEN}Control plane group: Name[${_CLR_YELLOW}$_WN_NAME${_CLR_GREEN}] Architecture[${_CLR_YELLOW}$_WN_ARCH${_CLR_GREEN}] Hyperthreading[${_CLR_YELLOW}$_WN_HYPERT${_CLR_GREEN}] Replicas[${_CLR_YELLOW}$_WN_REPL${_CLR_GREEN}]"
+    echo -e "${_CLR_GREEN}Control group: Name[${_CLR_YELLOW}$_WN_NAME${_CLR_GREEN}] Architecture[${_CLR_YELLOW}$_WN_ARCH${_CLR_GREEN}] Hyperthreading[${_CLR_YELLOW}$_WN_HYPERT${_CLR_GREEN}] Replicas[${_CLR_YELLOW}$_WN_REPL${_CLR_GREEN}]"
 
     # worker node groups
     _counter=0
@@ -132,10 +133,33 @@ showClusterConfiguration () {
       echo $_BM | jq -c '.hosts[]' | while read c; do
         _HNAME=$(echo "$c" | jq .name | sed 's/"//g')
         _HROLE=$(echo "$c" | jq .role | sed 's/"//g')
+
+        _NODE_STATUS=$(oc get node ${_HNAME} -o json)
+        _NODE_CAP_CPU=$(echo $_NODE_STATUS | jq .status.capacity.cpu)
+        _NODE_CAP_PODS=$(echo $_NODE_STATUS | jq .status.capacity.pods)
+        _NODE_ALLOC_CPU=$(echo $_NODE_STATUS | jq .status.allocatable.cpu)
+        _NODE_ALLOC_PODS=$(echo $_NODE_STATUS | jq .status.allocatable.pods)
+        _NUM_ACTIVE_PODS=$(oc get pods -A --no-headers --field-selector='status.phase=Running' --field-selector spec.nodeName=$_HNAME 2>/dev/null| grep Running | wc -l)
+
         _counter=$((_counter + 1))
-        echo -e "${_CLR_GREEN}    Name[${_CLR_YELLOW}$_HNAME${_CLR_GREEN}] Role[${_CLR_YELLOW}$_HROLE${_CLR_GREEN}]"
+        echo -e "${_CLR_GREEN}    Name[${_CLR_YELLOW}$_HNAME${_CLR_GREEN}] Role[${_CLR_YELLOW}$_HROLE${_CLR_GREEN}] CapacityCPUs[${_CLR_YELLOW}$_NODE_CAP_CPU${_CLR_GREEN}] CapacityPODs[${_CLR_YELLOW}$_NODE_CAP_PODS${_CLR_GREEN}] AllocatableCPUs[${_CLR_YELLOW}$_NODE_ALLOC_CPU${_CLR_GREEN}] AllocatablePODs[${_CLR_YELLOW}$_NODE_ALLOC_PODS${_CLR_GREEN}] RunningPODs[${_CLR_YELLOW}$_NUM_ACTIVE_PODS${_CLR_GREEN}]"
       done
 
+      # actual allocated resources
+      _pod_running_total=0
+      _allocatable_pod_running_total=0
+      for nodeName in `oc get nodes --no-headers --selector='node-role.kubernetes.io/worker=' | awk '{print $1}'`; do
+        _NUM_ACTIVE_PODS=$(oc get pods -A --no-headers --field-selector='status.phase=Running' --field-selector spec.nodeName=$nodeName 2>/dev/null| grep Running | wc -l)
+        _pod_running_total=$((_pod_running_total + $_NUM_ACTIVE_PODS))
+        _allocatable_on_node=$(oc get node $nodeName -o jsonpath='{.status.allocatable.pods}')
+        _allocatable_pod_running_total=$((_allocatable_pod_running_total + _allocatable_on_node))
+      done
+      echo -e "Total number of running pods on worker nodes [${_CLR_YELLOW}$_pod_running_total${_CLR_GREEN}] of total allocatables[${_CLR_YELLOW}$_allocatable_pod_running_total${_CLR_GREEN}]"
+      _APPROX_FREE_CPU=$((_allocatable_pod_running_total - _pod_running_total))
+      if [[ $_APPROX_FREE_CPU -lt 100 ]]; then
+        echo -e "${_CLR_YELLOW}WARNING you cluster is low on allocatable pods, free slots at approximately this time is near to ${_CLR_RED}$_APPROX_FREE_CPU${_CLR_NC}"
+      fi
+      oc adm top node --selector='node-role.kubernetes.io/worker=' 2>/dev/null
     fi
   fi
 }
@@ -143,3 +167,4 @@ showClusterConfiguration () {
 echo -e "=============================================================="
 echo -e "${_CLR_GREEN}Cluster configuration details${_CLR_NC}"
 showClusterConfiguration
+echo -e "${_CLR_NC}"
